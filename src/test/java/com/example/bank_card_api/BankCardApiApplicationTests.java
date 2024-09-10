dd6f581a0ee4e89945713a88074c301a0e1f2d93
hadiseShaaban <hadise.75.sh@gmail.com>
2024-09-10 13:54:23 +0330

    Initial commit

---
+++ src/test/java/com/example/bank_card_api/BankCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_card_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
